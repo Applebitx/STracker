b06b8a3e13d860ba533b3e0c1fda91d2afda466f
Applebitx <applebitx@gmail.com>
2022-11-11 12:21:12 +0300

    Initial Commit

---
+++ STracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  STracker
+//
+//  Created by Денис Сторожик on 11.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
